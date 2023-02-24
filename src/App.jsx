4dcf96145ae18fec7c701c887bb8506a9162dac8
dver26 <fercorvictor@gmail.com>
2023-02-24 15:39:14 +0100

    add mpas functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function HomePage() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <h1>Home</h1>
+      <p>Esta es una página de ejemplo para crear un react router desde cero</p>
+      <a href="/about">Ir a Sobre nosotros</a>
+    </>
   )
 }
 
-export default App
+export function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <div>
+        <img
+          src="https://pbs.twimg.com/profile_images/1580217302229860353/YqwVLbjs_400x400.jpg"
+          alt="Foto de Víctor"
+        />
+      </div>
+      <p>Hola, me llamo Víctor y estoy creando un clon de React Router.</p>
+      <a href="/">Ir a la Home</a>
+    </>
+  )
+}
+
+export default function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  return (
+    <main>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
+  )
+}
